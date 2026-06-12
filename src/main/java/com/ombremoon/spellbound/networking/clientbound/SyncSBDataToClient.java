ed8f5c45baad144dbad8a7aae264c7a73088cf54
Duck-XYZ <itsduckxyz@outlook.com>
2026-06-12 21:25:21 +0100

    Missing rituals fix

--- src/main/java/com/ombremoon/spellbound/networking/clientbound/SyncSBDataToClient.java
+++ src/main/java/com/ombremoon/spellbound/networking/clientbound/SyncSBDataToClient.java
@@ -1,8 +1,10 @@
 package com.ombremoon.spellbound.networking.clientbound;
 
 import com.ombremoon.spellbound.common.magic.acquisition.guides.GuideBookPage;
+import com.ombremoon.spellbound.common.magic.acquisition.transfiguration.TransfigurationRitual;
 import com.ombremoon.spellbound.main.CommonClass;
 import io.netty.buffer.ByteBuf;
+import net.minecraft.core.Holder;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.network.protocol.common.custom.CustomPacketPayload;
@@ -13,12 +15,13 @@ import java.util.List;
 import java.util.Map;
 import java.util.ResourceBundle;
 
-public record SendGuideBooksPayload(Map<ResourceLocation, List<GuideBookPage>> pages, Map<ResourceLocation, Integer> pageIndex) implements CustomPacketPayload {
+public record SendGuideBooksPayload(Map<ResourceLocation, List<GuideBookPage>> pages, Map<ResourceLocation, Integer> pageIndex, Map<ResourceLocation, TransfigurationRitual> rituals) implements CustomPacketPayload {
     public static final Type<SendGuideBooksPayload> TYPE = new Type<>(CommonClass.customLocation("send_guide_books_payload"));
 
     public static final StreamCodec<ByteBuf, SendGuideBooksPayload> STREAM_CODEC = StreamCodec.composite(
             ByteBufCodecs.map(HashMap::new, ResourceLocation.STREAM_CODEC, ByteBufCodecs.fromCodec(GuideBookPage.CODEC).apply(ByteBufCodecs.list())), SendGuideBooksPayload::pages,
             ByteBufCodecs.map(HashMap::new, ResourceLocation.STREAM_CODEC, ByteBufCodecs.INT), SendGuideBooksPayload::pageIndex,
+            ByteBufCodecs.map(HashMap::new, ResourceLocation.STREAM_CODEC, ByteBufCodecs.fromCodec(TransfigurationRitual.DIRECT_CODEC)), SendGuideBooksPayload::rituals,
             SendGuideBooksPayload::new
     );
 
